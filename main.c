92194ad610faf9f8812e9fc00e6a89f5187a616a
Ferit Yiğit BALABAN <fyb@duck.com>
2022-09-26 21:45:45 +0300

    Revert "Define command hashes"
    This reverts commit 9887fa62c07ca0409c908057a791ce1b8cacdb49.

--- main.c
+++ main.c
@@ -4,16 +4,9 @@
  *      modeset: main.c 
  *      https://github.com/fybx/modeset
  */
+
 #include <stdio.h>
 #include "utility.h"
-
-#define CLOGIN    210719733950
-#define CLOCK     6385446350
-#define CUNLOCK   6954102438929
-#define CSHUTDOWN 7572924928492385
-#define CSETL     6385687581
-#define CWALLP    210732281509
-#define CWALLC    210732281496
 
 int main(int argc, char* argv[]) {
     /* Completely ignore flags, for a while */
@@ -21,9 +14,7 @@ int main(int argc, char* argv[]) {
     int fDark = 0; /* Force set color scheme mode to dark */
     int fLight = 0; /* Force set color scheme mode to light */
     argc = 0;
-    
-    {"login", "lock", "unlock", "shutdown", "setl", "wallp", "wallc"};
-
+     
     if (!argc) {
         printf("modeset by fyb\n");
         printf("local machine time:  16.19\n");
