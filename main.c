2cdc799d4945a38efaf58e283488333e4d423202
Ferit Yiğit BALABAN <fyb@duck.com>
2022-09-26 21:48:10 +0300

    Define command hashes

--- main.c
+++ main.c
@@ -4,9 +4,16 @@
  *      modeset: main.c 
  *      https://github.com/fybx/modeset
  */
-
 #include <stdio.h>
 #include "utility.h"
+
+#define CLOGIN    210719733950
+#define CLOCK     6385446350
+#define CUNLOCK   6954102438929
+#define CSHUTDOWN 7572924928492385
+#define CSETL     6385687581
+#define CWALLP    210732281509
+#define CWALLC    210732281496
 
 int main(int argc, char* argv[]) {
     /* Completely ignore flags, for a while */
